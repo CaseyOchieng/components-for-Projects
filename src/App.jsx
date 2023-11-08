faa9e458449cdc1e2a9cda3e583cbe6bdb0f59c9
casey <caseyochieng46@gmail.com>
2023-11-09 00:06:55 +0300

    Added a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import Card1 from '../src/Components/card1'
+import Card2 from '../src/Components/card2'
+import Card3 from '../src/Components/card3'
 
 import Navbar from '../src/Components/navbar'
 import '../src/css/App.css'
@@ -6,6 +9,10 @@ function App() {
   return (
     <div>
     <Navbar/>
+    <Card1/>
+    <Card2/>
+    <Card3/>
+
     </div>
   )
 }
