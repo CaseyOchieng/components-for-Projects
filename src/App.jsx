38e70b8a9eb091f2b57425d16edc3b312a44770f
casey <caseyochieng46@gmail.com>
2023-11-09 14:17:05 +0300

    Added a

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,86 @@ import Card3 from '../src/Components/card3'
 
 import Navbar from '../src/Components/navbar'
 import '../src/css/App.css'
+import Hero from './Components/hero'
 function App() {
 
   return (
     <div>
     <Navbar/>
+    <Hero/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <hr/>
+    <br/>
+
     <Card1/>
     <Card2/>
     <Card3/>
---
+++ src/Components/hero.jsx
@@ -0,0 +1,17 @@
+
+function Hero() {
+  return (
+<section className="p-4">
+<div className="bg-violet-400 p-8">
+    {/* First div */}
+      <div>
+    {/* Second div */}
+      </div>
+    {/* Third div */}
+      <div></div>
+    </div>
+</section>
+  );
+}
+
+export default Hero;
